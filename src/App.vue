bf2827c15f45b96994ab0ae1bffb297fa1d4aa19
= <=>
2020-09-19 10:15:47 -0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+<v-app>
+
+<Toolbar/>
+<v-main>
+      <Search v-on:searchResults="searchResult"/>
+      <ImageList v-bind:items="items"/>
+</v-main>
+<h1>Funciona</h1>
+</v-app>
+</template>
+
+<script>
+
+import Toolbar from './components/Toolbar.vue';
+import Search from "./components/Search";
+import ImageList from "./components/ImageList";
+
+
+export default {
+  name: 'App',
+
+  components: {
+    Toolbar,
+    Search,
+    ImageList
+  },
+
+  methods: {
+    searchResult(response){
+      this.items = response;
+      console.log(this.items)
+    }
+  },
+
+  data: () => ({
+    items: [],
+  }),
+};
+</script>
